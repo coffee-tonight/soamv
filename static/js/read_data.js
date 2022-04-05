ed9176df2e242c0ef88c164811a40d21387ac2d6
Bishant <bishant.nayak44@gmail.com>
2022-04-06 01:49:37 +0530

    created the slicing of recieved data and printed

--- static/js/read_data.js
+++ static/js/read_data.js
@@ -156,8 +156,8 @@ var sheetName = {
 }
 
 async function read_vehicle_data(vh_num) {
-    var sheet = sheetName[vh_num]
-    console.log(sheet);
+    // var sheet = sheetName[vh_num]
+    // console.log(sheet);
 
     var params = {
         // spreadsheetId: '1_WKqPRuOArfiQ0JeUuiooz67vjCsoLDYA7xuT2izycg',
@@ -168,7 +168,7 @@ async function read_vehicle_data(vh_num) {
 
     var request = gapi.client.sheets.spreadsheets.values.get(params);
     request.then(function(response) {
-        console.log(`Result Fetched for ${sheet}: `, response.result);
+        // console.log(`Result Fetched for ${sheet}: `, response.result);
         processResult(response.result);
         // return response.result;
     }, function(reason) {
@@ -178,21 +178,36 @@ async function read_vehicle_data(vh_num) {
 }
 
 function processResult(data) {
-    console.log("Data in Process Result: ", data);
+    // console.log("Data in Process Result: ", data);
     const values = data.values;
-    var vh_details = values.slice(0, 8);
-    var maintenance_records = values.slice(8);
-    var maintenance_records1 = values.slice(12);
-    var maintenance_records2 = values.slice(8);
 
-    console.log("details: vh: ", vh_details, "MR: ", maintenance_records, maintenance_records1, maintenance_records2);
+    // Decrease the lower limit of the range to ommit the header row
+    var vh_details = values.slice(3, 9);
+    var assignment_details = values.slice(9, 16);
+    var insurance_details = values.slice(17, 21);
+    var fitness_details = values.slice(22, 25);
+    var permit_details = values.slice(26, 30);
+    var puc_details = values.slice(30, 32);
+    var battery_details = values.slice(33, 37);
+    var tyre_details = values.slice(38, 46);
+    var maintenance_records = values.slice(51);
+
+    console.log("Vehicle Details: ", vh_details);
+    console.log("Assignment Details: ", assignment_details);
+    console.log("Insurance Details: ", insurance_details);
+    console.log("Fitness Details: ", fitness_details);
+    console.log("Permit Details: ", permit_details);
+    console.log("PUC Details: ", puc_details);
+    console.log("Battery Details: ", battery_details);
+    console.log("Tyre Details: ", tyre_details);
+    console.log("Maintenance Records: ", maintenance_records);
     
 }
 
 async function search_vehicle() {
     search_val = document.getElementById("search").value;
     if (search_val != "") {
-        console.log("Hey! I'm in search_vehicle()", content);
+        // console.log("Hey! I'm in search_vehicle()", content);
         content.style.position = "absolute";
         content.style.top = "100px";
         content.style.transition = "2.4s";

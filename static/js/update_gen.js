e2842de158f48827b946e212c7d3b10d2b2fe76f
Bishant <bishant.nayak44@gmail.com>
2022-04-11 18:05:44 +0530

    Updated the update_gen

--- static/js/update_gen.js
+++ static/js/update_gen.js
@@ -22,7 +22,7 @@ async function read_vehicle_data(vh_num) {
 
 function processResult(result) {
     let array_rows = result.values.length;
-    console.log("Arrays_rows: ", array_rows);
+    // console.log("Arrays_rows: ", array_rows);
     var par_table = document.getElementById("update_content");
   
     var table = document.createElement("table");
@@ -36,24 +36,30 @@ function processResult(result) {
   
     console.log("Values: ", result.values); 
     for(var row=0; row<array_rows; row++) {
-      var tr = document.createElement("tr");
-    //   tr.classList.add("");
-      tableBody.appendChild(tr);
-  
-      console.log("Row: ", result.values[row]);
-  
-      result.values[row].forEach(el => {
-        var td = document.createElement("TD");
-        td.classList.add("td_");
-        var val = document.createElement("INPUT");
-        val.setAttribute("type", "text");
-        val.setAttribute("value", el);
-        td.appendChild(val);
-        tr.appendChild(td);
-      });
-  
-      par_table.appendChild(table);
+        var tr = document.createElement("tr");
+        //   tr.classList.add("");
+        tableBody.appendChild(tr);
+    
+        // console.log("Row: ", result.values[row]);
+    
+        result.values[row].forEach(el => {
+            var td = document.createElement("TD");
+            td.classList.add("td_");
+            var val = document.createElement("INPUT");
+            val.classList.add("input_");
+            val.setAttribute("type", "text");
+            val.setAttribute("value", el);
+            td.appendChild(val);
+            tr.appendChild(td);
+        });
+    
+        par_table.appendChild(table);
     }
+    let btn = document.createElement("button");
+    btn.classList.add("btn_");
+    btn.setAttribute("onclick", "update_data()");
+    btn.innerHTML = "Update";
+    par_table.appendChild(btn);
 }
 
 async function search_vehicle() {
@@ -62,9 +68,18 @@ async function search_vehicle() {
     if (search_val != "") {
         console.log("Hey! I'm in search_vehicle()", content);
         content.style.position = "absolute";
-        content.style.top = "100px";
-        content.style.transition = "2.4s";
+        content.style.top = "50px";
+        content.style.transition = "2s";
         var data = await read_vehicle_data(search_val);
         // console.log("Data returned: ", data);
     }
+}
+
+search_val = document.getElementById("search").value;
+if (search_val != "") {
+    search_val.style.width = "250px";
+}
+
+function update_data() {
+    console.log("Hey! I'm in update_data()");
 }
